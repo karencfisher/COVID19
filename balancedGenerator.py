b29a231168c17a4f5e50645d4a3b3c971182fefe
karencfisher <karencfisher@hotmail.com>
2021-08-23 14:18:11 -0600

    add undersampling option

--- balancedGenerator.py
+++ balancedGenerator.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 
 from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
 
 
-def balancedGenerator(data_path, classes, datagen, target_size, 
+def balancedGenerator(data_path, classes, datagen, target_size, strategy='under',
                         batch_size=32, class_mode='categorical', random_state=42):
     '''
-    Oversampling of non-majority class from directories of images.
+    Resamples image data to either undersample non-minority classes or oversample
+    non-majority classes. Returns a data generator. 
 
     Given a directory structure comtaining subdirectories for images divided into 
     classes, e.g.
@@ -30,9 +32,13 @@ def balancedGenerator(data_path, classes, datagen, target_size,
     datagen: instance of Keras ImageDataGenerator. Can include data
              augmentation options.
     target_size: H/W target dimensions of images. Tuple. (e.g., (299, 299))
-    batch_size: batch size
+    strategy: "under" to undersample non-minority classes or "over" to 
+              oversample the non-majority classes. Default is "under".
+    batch_size: batch size. Default 32
     class_mode: class mode for Keras' flow_from_dataframe method. E.g.,
                 'binary', 'category', etc. See Keras documentation.
+                default is 'categorical'
+    random_state: random seed. Default is 42.
 
     Returns: the generator to be used in training a model.
     '''
@@ -48,10 +54,13 @@ def balancedGenerator(data_path, classes, datagen, target_size,
         y += [label] * len(class_files)
 
     # Oversample non-majority classes (using imblearn)
-    ros = RandomOverSampler(random_state=random_state)
+    if strategy == 'over':
+        resample = RandomOverSampler(random_state=random_state)
+    else:
+        resample = RandomUnderSampler(random_state=random_state)
     X = np.array(X).reshape(-1, 1)
     y = np.array(y).reshape(-1, 1)
-    X_res, y_res = ros.fit_resample(X, y)
+    X_res, y_res = resample.fit_resample(X, y)
     X_res = X_res.reshape(-1)
 
     # make into a dataframe
